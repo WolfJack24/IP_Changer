e6f244b60b6ccd2a6f94b2538a24b203ea5e8185
WolfJack <liamschroder1@gmail.com>
2026-08-20 13:23:34 +0200

    Added a way to list all the available adapters

--- Commands/ListAdaptersCommand.cs
+++ Commands/ListAdaptersCommand.cs
@@ -1,3 +1,5 @@
+using IP_Changer.Services;
+using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace IP_Changer.Commands
@@ -10,7 +12,30 @@ namespace IP_Changer.Commands
 
         protected override int Execute(CommandContext context, Settings settings, CancellationToken cancellationToken)
         {
-            Console.WriteLine("TODO: List all adapters");
+            if (context.Data is AdapterService)
+            {
+                var adapters = AdapterService.GetAdapters();
+
+                var table = new Table().RoundedBorder();
+                table.AddColumn("Name");
+                table.AddColumn("Description");
+                table.AddColumn("IsPhysical");
+                table.AddColumn("Type");
+
+                for (int i = 0; i < adapters.Count - 1; i++)
+                {
+                    var adapter = adapters[i];
+                    table.AddRow(
+                        Markup.Escape(adapter.Name ?? string.Empty),
+                        Markup.Escape(adapter.Description ?? string.Empty),
+                        Markup.Escape(adapter.IsPhysical.ToString()),
+                        Markup.Escape(adapter.Type.ToString())
+                    );
+                }
+
+                AnsiConsole.Write(table);
+            }
+
             return 0;
         }
     }
