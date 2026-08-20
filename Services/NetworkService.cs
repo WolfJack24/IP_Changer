84f40d63160f64e89add37e6e71abae2b3442303
WolfJack <liamschroder1@gmail.com>
2026-08-20 13:21:55 +0200

    Cleaned NetworkService

--- Services/NetworkService.cs
+++ Services/NetworkService.cs
@@ -4,22 +4,18 @@ namespace IP_Changer.Services
 {
     public class NetworkService
     {
-        public NetworkConfiguration GetConfiguration(
-            NetworkAdapter adapter)
+        public NetworkConfiguration GetConfiguration(NetworkAdapter adapter)
         {
             // Implementation for getting network configuration
             return new NetworkConfiguration();
         }
 
-        public void SetDhcp(
-            NetworkAdapter adapter)
+        public void SetDhcp(NetworkAdapter adapter)
         {
             // Implementation for setting DHCP
         }
 
-        public void SetStatic(
-            NetworkAdapter adapter,
-            NetworkProfile profile)
+        public void SetStatic(NetworkAdapter adapter, NetworkProfile profile)
         {
             // Implementation for setting static IP
         }
