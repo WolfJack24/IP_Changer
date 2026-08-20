5c2bbcd85c55a2e4c562a34eddf8a8d785a2421e
WolfJack <liamschroder1@gmail.com>
2026-08-20 13:22:34 +0200

    Updated NetworkAdapter class

--- Models/NetworkAdapter.cs
+++ Models/NetworkAdapter.cs
@@ -5,8 +5,10 @@ namespace IP_Changer.Models
     public class NetworkAdapter
     {
         public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public string Id { get; set; } = string.Empty;
 
+        public bool IsPhysical { get; set; }
         public bool IsEnabled { get; set; }
         public bool IsConnected { get; set; }
 
