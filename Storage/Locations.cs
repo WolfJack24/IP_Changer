12f9c6c1c82e7dd30b0911edf17a4e3e35591ce5
WolfJack24 <111003236+WolfJack24@users.noreply.github.com>
2026-08-20 23:33:53 +0200

    Added File Location Management

---
+++ Storage/Locations.cs
@@ -0,0 +1,10 @@
+﻿namespace IP_Changer.Storage
+{
+    public class Locations
+    {
+        private static readonly string _localAppdata = Environment.GetEnvironmentVariable("LOCALAPPDATA") ?? throw new Exception("Where is Local App Data?!");
+        public static readonly string storageLocation = Path.Combine(_localAppdata, "IP_Changer");
+        public static readonly string profileLocation = Path.Combine(storageLocation, "Profiles.json");
+        public static readonly string adapterLocation = Path.Combine(storageLocation, "Adapters.json");
+    }
+}
---
+++ Storage/OnLoad.cs
@@ -0,0 +1,17 @@
+﻿namespace IP_Changer.Storage
+{
+    public class OnLoad
+    {
+        public static void CheckLocations()
+        {
+            if (!Path.Exists(Locations.storageLocation))
+                Directory.CreateDirectory(Locations.storageLocation);
+
+            if (!File.Exists(Locations.profileLocation))
+                File.Create(Locations.profileLocation);
+
+            if (!File.Exists(Locations.adapterLocation))
+                File.Create(Locations.adapterLocation);
+        }
+    }
+}
