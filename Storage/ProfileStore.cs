990e89051e361d1d3148b46807290b60cbef1531
WolfJack <liamschroder1@gmail.com>
2026-08-21 12:28:33 +0200

    Added Profile saving and loading

--- Storage/ProfileStore.cs
+++ Storage/ProfileStore.cs
@@ -1,18 +1,43 @@
-﻿using IP_Changer.Models;
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text.Json;
+using IP_Changer.Models;
+using Spectre.Console;
 
 namespace IP_Changer.Storage
 {
     public class ProfileStore
     {
-        public List<NetworkProfile> Load()
+        public List<NetworkProfile> profiles = [];
+        private readonly JsonSerializerOptions options = new() { WriteIndented = true };
+
+        public (List<NetworkProfile>, bool) Load()
         {
-            // Read JSON
-            return [];
+            try
+            {
+                string jsonString = File.ReadAllText(Locations.profileLocation);
+                var profilesList = JsonSerializer.Deserialize<List<NetworkProfile>>(jsonString);
+
+                if (profilesList is not null) return (profilesList, true);
+                else return ([], false);
+            }
+            catch (JsonException ex)
+            {
+                // AnsiConsole.WriteLine($"Exception: {ex.Message}");
+                _ = ex;
+                return ([], false);
+            }
         }
 
-        public void Save(List<NetworkProfile> profiles)
+        public void Save(NetworkProfile profile)
         {
-            // Write JSON
+            if (profiles.Any(p => p.Name.Equals(profile.Name, StringComparison.OrdinalIgnoreCase)))
+            {
+                AnsiConsole.MarkupLine($"[red]You already have a profile with the name of [blue]{profile.Name}[/][/]");
+                return;
+            }
+
+            profiles.Add(profile);
+            File.WriteAllText(Locations.profileLocation, JsonSerializer.Serialize(profiles, options));
         }
     }
 }
