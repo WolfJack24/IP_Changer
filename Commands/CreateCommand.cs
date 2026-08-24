fcd02353de408e0dcaad27d181f5142494c59653
WolfJack24 <111003236+WolfJack24@users.noreply.github.com>
2026-08-24 23:14:07 +0200

    Clean up the CreateCommand

--- Commands/CreateCommand.cs
+++ Commands/CreateCommand.cs
@@ -1,7 +1,5 @@
 ﻿using IP_Changer.Enums;
-using IP_Changer.Models;
 using IP_Changer.Services;
-using IP_Changer.Storage;
 using Spectre.Console;
 using Spectre.Console.Cli;
 using System.ComponentModel;
@@ -19,7 +17,7 @@ namespace IP_Changer.Commands
 
         protected override int Execute(CommandContext context, Settings settings, CancellationToken cancellationToken)
         {
-            if (context.Data is ProfileStore profileStore)
+            if (context.Data is ProfileService profileService)
             {
                 AnsiConsole.MarkupLine($"Creating a profile with the name: [blue]{settings.ProfileName}[/]\n");
 
@@ -32,7 +30,7 @@ namespace IP_Changer.Commands
                         .AddChoices(choices)
                 );
 
-                var adapter = AdapterService.GetAdapter(adapterName);
+                var adapter = adapters.Find(a => a.Name == adapterName) ?? throw new Exception("Adapter can not be null");
 
                 AnsiConsole.MarkupLine($"Modifying: [blue]{adapterName}[/]");
 
@@ -54,108 +52,45 @@ namespace IP_Changer.Commands
 
                 switch (networkMode)
                 {
-                    case NetworkMode.DHCP: break;
+                    case NetworkMode.DHCP:
+                        {
+                            var profile = profileService.CreateDHCPProfile(
+                                    settings.ProfileName,
+                                    adapter,
+                                    networkMode);
+
+                            AnsiConsole.Status()
+                                .Start("Saving Profile...", ctx =>
+                                {
+                                    profileService.SaveProfile(profile);
+                                    AnsiConsole.MarkupLine("[green]Profile saved![/]");
+                                });
+                        }
+                        break;
                     case NetworkMode.Static:
                         {
-                            if (AnsiConsole.Confirm("Will there be multiple IPs?"))
-                            {
-                                var numOfIp = AnsiConsole.Ask<int>("How many?");
+                            var numOfIp = AnsiConsole.Prompt(
+                                new TextPrompt<int>("How many IPs?")
+                                    .Validate(
+                                        n => n is >= 2 and <= 5
+                                        ? ValidationResult.Success()
+                                        : ValidationResult.Error("[red]Please enter a number between 2 and 5.[/]")));
 
-                                if (numOfIp >= 2 && numOfIp <= 5)
+                            var profile = profileService.CreateStaticProfile(
+                                    settings.ProfileName,
+                                    adapter,
+                                    networkMode,
+                                    numOfIp);
+                            profileService.SaveProfile(profile);
+
+                            AnsiConsole.Status()
+                                .Start("Saving Profile...", ctx =>
                                 {
-                                    for (int i = 0; i < numOfIp; i++)
-                                    {
-                                        Ip = AnsiConsole.Ask<string>("IP:");
-                                        Subnet = AnsiConsole.Ask<string>("Subnet:");
-
-                                        Ips.Add(i, Ip);
-                                        Subnets.Add(i, Subnet);
-                                    }
-
-                                    Gateway = AnsiConsole.Ask<string>("Gateway:");
-
-                                    AnsiConsole.WriteLine();
-
-                                    if (AnsiConsole.Confirm("Set DNS automatically?"))
-                                    {
-                                        PriDns = Gateway; // TODO: do better
-                                    }
-                                    else
-                                    {
-                                        AnsiConsole.WriteLine();
-
-                                        PriDns = AnsiConsole.Ask<string>("Primary DNS:");
-                                        SecDns = AnsiConsole.Ask<string>("Secondary DNS:");
-                                    }
-
-                                    var profile = new NetworkProfile()
-                                    {
-                                        Name = settings.ProfileName,
-                                        Adapter = adapter,
-                                        Mode = networkMode,
-                                        IpAddress = Ips,
-                                        SubnetMask = Subnets,
-                                        Gateway = Gateway,
-                                        DnsServers = [PriDns, SecDns]
-                                    };
-
-                                    AnsiConsole.Status()
-                                        .Start("Saving Profile...", ctx =>
-                                        {
-                                            profileStore.Save(profile);
-                                            AnsiConsole.MarkupLine("[green]Profile saved![/]");
-                                        });
-                                }
-                                else
-                                {
-                                    AnsiConsole.MarkupLine($"[red]Can not have this many Ips: [blue]{numOfIp}[/], must be between 2 and 5[/]");
-                                }
-                            }
-                            else
-                            {
-                                AnsiConsole.WriteLine();
-
-                                Ip = AnsiConsole.Ask<string>("IP:");
-                                Subnet = AnsiConsole.Ask<string>("Subnet:");
-                                Gateway = AnsiConsole.Ask<string>("Gateway:");
-
-                                Ips.Add(0, Ip);
-                                Subnets.Add(0, Subnet);
-
-                                AnsiConsole.WriteLine();
-
-                                if (AnsiConsole.Confirm("Set DNS automatically?"))
-                                {
-                                    PriDns = Gateway; // TODO: do better
-                                }
-                                else
-                                {
-                                    AnsiConsole.WriteLine();
-
-                                    PriDns = AnsiConsole.Ask<string>("Primary DNS:");
-                                    SecDns = AnsiConsole.Ask<string>("Secondary DNS:");
-                                }
-
-                                var Profile = new NetworkProfile()
-                                {
-                                    Name = settings.ProfileName,
-                                    Adapter = adapter,
-                                    Mode = networkMode,
-                                    IpAddress = Ips,
-                                    SubnetMask = Subnets,
-                                    Gateway = Gateway,
-                                    DnsServers = [PriDns, SecDns]
-                                };
-
-                                AnsiConsole.Status()
-                                    .Start("Saving Profile...", ctx =>
-                                    {
-                                        profileStore.Save(Profile);
-                                        AnsiConsole.MarkupLine("[green]Profile saved![/]");
-                                    });
-                            }
-                            break;
+                                    profileService.SaveProfile(profile);
+                                    AnsiConsole.MarkupLine("[green]Profile saved![/]");
+                                });
                         }
+                        break;
                 }
             }
             return 0;
